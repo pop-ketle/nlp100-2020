4fcac63771fdaf6a8b9dfc343541ae7d1f881235
pop-ketle <popqjim@gmail.com>
2020-04-19 20:08:42 +0900

    update ans12

--- ch2:UNIXコマンド/ans12.py
+++ ch2:UNIXコマンド/ans12.py
@@ -5,5 +5,5 @@
 import pandas as pd
 
 df = pd.read_csv('./popular-names.txt', sep='\t', header=None)
-df[0].to_csv('./col1.txt')
-df[1].to_csv('./col2.txt')
+df[0].to_csv('./col1.txt', index=False, header=None)
+df[1].to_csv('./col2.txt', index=False, header=None)
