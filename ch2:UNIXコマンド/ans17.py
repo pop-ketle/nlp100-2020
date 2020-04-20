76590dc52f4512a50eb5f4265e6c3e7b428fd48c
pop-ketle <popqjim@gmail.com>
2020-04-20 23:34:57 +0900

    add ans17

---
+++ ch2:UNIXコマンド/ans17.py
@@ -0,0 +1,7 @@
+# 17. １列目の文字列の異なり
+# 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはcut, sort, uniqコマンドを用いよ．
+
+import pandas as pd
+
+df = pd.read_csv('./popular-names.txt', sep='\t', header=None)
+print(set(df[0]))
